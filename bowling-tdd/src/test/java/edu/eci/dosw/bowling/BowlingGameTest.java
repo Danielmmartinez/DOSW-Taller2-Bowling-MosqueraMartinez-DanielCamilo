8939c1d3113fd35582049a78d57aa84ec93b3109
Daniel Mosquera <daniel.mmartinez@mail.escuelaing.edu.co>
2026-09-15 19:00:49 -0500

    test: RED - roll(11) lanza IllegalArgumentException

--- bowling-tdd/src/test/java/edu/eci/dosw/bowling/BowlingGameTest.java
+++ bowling-tdd/src/test/java/edu/eci/dosw/bowling/BowlingGameTest.java
@@ -17,5 +17,11 @@ class BowlingGameTest {
         BowlingGame game = new BowlingGame();
         assertThrows(IllegalArgumentException.class, () -> game.roll(-1));
     }
+    @Test
+    @DisplayName("A3 - roll(11) lanza IllegalArgumentException")
+    void rollMoreThan10Pins_shouldThrowException() {
+        BowlingGame game = new BowlingGame();
+        assertThrows(IllegalArgumentException.class, () -> game.roll(11));
+    }
 
 }
